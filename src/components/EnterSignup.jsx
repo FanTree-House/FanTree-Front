3fbb219e6d8029a214c37961f78f290aba9f025e
silicao3o <zergskybmw@gmail.com>
2024-08-08 18:41:09 +0900

    [Fix] 회원가입 오류 수정

--- src/components/EnterSignup.jsx
+++ src/components/EnterSignup.jsx
@@ -38,8 +38,8 @@ const SignupForm = () => {
   };
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     if (availableId && availableNickname && availableEmail && availablePassword){
-      e.preventDefault();
       const formDataToSend = new FormData();
       for (const key in formData) {
         formDataToSend.append(key, formData[key]);
@@ -90,7 +90,7 @@ const SignupForm = () => {
   const handleCheckPassword = async () =>{
     try {
       const data = await verifyPassword(formData.password, formData.checkPassword);
-      setAvailavblePassowrd(!data.result)
+      setAvailavblePassowrd(data.result)
       alert(data.message)
     }catch (error){
       alert('서버에서 오류가 발생했습니다. 다시 시도해주세요');
@@ -109,7 +109,7 @@ const SignupForm = () => {
   const handleVerifyAuthNumber = async () => {
     try {
       const data = await verifyAuthNumber(formData.id, formData.email, formData.authNumber);
-      setAvailableEmail(!data.result)
+      setAvailableEmail(data.result)
       alert(data.message)
     } catch (error) {
       alert('서버에서 오류가 발생했습니다. 다시 시도해주세요');
