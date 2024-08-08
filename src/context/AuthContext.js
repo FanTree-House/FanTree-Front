f7dca80ccba4ce31fdaac07637889579abdeaa92
Gaeun <rkdms1111111@naver.com>
2024-08-08 22:36:23 +0900

    [Chore] 주석 한글로

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -43,7 +43,7 @@ export function AuthProvider({ children }) {
 export function useAuthState() {
     const context = useContext(AuthStateContext);
     if (context === undefined) {
-        throw new Error('useAuthState must be used within a AuthProvider');
+        throw new Error('useAuthState는 AuthProvider 내에서 사용해야 합니다.');
     }
     return context;
 }
@@ -51,7 +51,7 @@ export function useAuthState() {
 export function useAuthDispatch() {
     const context = useContext(AuthDispatchContext);
     if (context === undefined) {
-        throw new Error('useAuthDispatch must be used within a AuthProvider');
+        throw new Error('useAuthDispatch는 AuthProvider 내에서 사용해야 합니다.');
     }
     return context;
 }
