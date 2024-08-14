c1c78d02630dc40e88c18ed3af5be1810d22a94f
Gaeun <rkdms1111111@naver.com>
2024-08-14 09:20:43 +0900

    [Chore] 설문폼 연결

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -86,7 +86,7 @@ function LoginPage() {
           />
         </div>
         <div className="links">
-          <Link to="/signup">회원가입</Link> | <Link to="/">문의하기</Link> | <Link to="https://www.notion.so/teamsparta/ID-5a77ff084e224d408d2f1b00560f89c1">아이디 찾기</Link>
+          <Link to="/signup">회원가입</Link> | <Link to="https://forms.gle/Uk1qPKahSLLB42m66">문의하기</Link> | <Link to="https://www.notion.so/teamsparta/ID-5a77ff084e224d408d2f1b00560f89c1">아이디 찾기</Link>
         </div>
       </div>
   );
